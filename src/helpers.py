26db15f8708e8a0bce5f9d9e10a42e5a51dd7883
tudorsonycx <tudorsonycx@gmail.com>
2024-12-09 17:00:51 +0000

    add get_date helper method

---
+++ src/helpers.py
@@ -0,0 +1,15 @@
+from datetime import datetime as dt
+
+FORMAT = "%d-%m-%y"
+CATEGORIES = {"I": "Income", "E": "Expense"}
+
+
+def get_date(today=False):
+    if today:
+        return dt.today().strftime(FORMAT)
+    while not today and True:
+        try:
+            date_prompt = input("Enter date in the format 'DD-MM-YY': ")
+            return dt.strptime(date_prompt, FORMAT).strftime(FORMAT)
+        except ValueError:
+            print("Wrong format! Try again.")
